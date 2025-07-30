5fe8f71a007a72bcb1a7712741514b133fa84798
DulaKavindi <dularikavindi6@gmail.com>
2025-07-30 12:53:27 +0530

    Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Welcome to PSR Vlog</h1>
+      <button>Users</button>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/userForm.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Grid, Input, Typography} from "@mui/material";
+
+function userForm(props) {
+  return (
+   <Grid
+    container
+    spacing={2}
+    sx={{
+        backgrounColor: 'white',
+        marginBottom: '20px',
+        display: 'block',
+    }}>
+        <Grid item xs={12}>
+            <Typography component={'h1'} 
+            sx={{color: '#000000'}}>User Form</Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={6} sx={{display: 'flex'}}>
+            <Typography component={'label'}
+            htmlFor="id"
+            sx={{
+                color: '#000000',
+                marginRight: '20px',
+                fontSize: '100px',
+                width: '100px',
+                display: 'block'
+            }}>ID</Typography>
+
+            <Input
+                type ="number"
+                id='id'
+                name="id"
+                sx={{
+                    width: '400px'
+                }}
+                value={''}
+                onChange={e => {}}/>
+        </Grid>
+    
+   </Grid>
+  )
+}
+
+export default userForm
