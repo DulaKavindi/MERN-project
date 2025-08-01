ec8cdc5294fd10e871002133bddee58a4c742c84
DulaKavindi <dularikavindi6@gmail.com>
2025-08-01 12:49:18 +0530

    Backend

--- backend/controller.js
+++ backend/controller.js
@@ -1,23 +1,29 @@
-const users = [
-    {
-        id: 1,
-        name: 'Dulari',
-    },
-    {
-        id:2,
-        name: 'Kavindi',
-    }
-];
+const User = required('./model');
 
-const getUsers = (cb) => {
-    cb(users);
+const getUsers = (req, res, next) => {
+    User.find()
+        .then(response =>{
+            res.json({response})
+        })
+        .catch(error =>{
+            res.json({message: error})
+        })
+};
+
+const addUser= (req, res, next)=>{
+    const user = new User({
+        id: req.body.id,
+        name: req.body.name,
+    });
+    User.save()
+    .then(response =>{
+            res.json({response})
+        })
+        .catch(error =>{
+            res.json({message: error})
+        })
 }
 
-const getUsersById = (id, cb)=> {
-    const user = users.find(user => user.id == id);
-
-    cb(user);
-}
 
 exports.getUsers = getUsers;
 exports.getUsersById = getUsersById;
